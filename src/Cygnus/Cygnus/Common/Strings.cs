e1d871eee93b017cdb66f4ccad13578f54dbee20
Denis Kaynarca <denis@dkaynarca.com>
2015-03-09 17:01:08 +1100

    + Updated .gitignore to skip private configuration data + Added core project + Added user account functionality + Added email confirmation and password reset

---
+++ src/Cygnus/Cygnus/Common/Strings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Cygnus.Common
+{
+    public static class Strings
+    {
+        public const string ADMIN_EMAIL_ADDRESS = "cygnus@dkaynarca.com";
+        public const string SMTP_SERVER = "mail.dkaynarca.com";
+        public const Int32 SMTP_PORT = 587;
+    }
+}
