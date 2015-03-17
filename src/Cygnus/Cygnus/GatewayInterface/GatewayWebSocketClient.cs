ee9f75203bc647e799ca248c9285b48051150529
Denis Kaynarca <denis@dkaynarca.com>
2015-03-17 16:47:21 +1100

    + Added placeholders and basic logic for the gateway interface web socket client.

---
+++ src/Cygnus/Cygnus/GatewayInterface/GatewayWebSocketClient.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Web;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Cygnus.GatewayInterface
+{
+    public class GatewayWebSocketClient
+    {
+        private ClientWebSocket m_gatewayWebSocket = new ClientWebSocket();
+        
+        public GatewayWebSocketClient()
+        {
+        }
+
+        public async void Connect(string uri)
+        {
+            if (m_gatewayWebSocket.State == WebSocketState.Closed && !String.IsNullOrEmpty(uri))
+            {
+               var ct = new CancellationToken();
+               await m_gatewayWebSocket.ConnectAsync(new Uri(uri), ct);
+            }
+        }
+
+        public async void Disconnect()
+        {
+            var ct = new CancellationToken();
+            await m_gatewayWebSocket.CloseAsync(WebSocketCloseStatus.NormalClosure,
+                                                "Normal", ct);
+        }
+
+        public async Task<ISensorData> QuerySensorData(Guid sensorId)
+        {
+            var ct = new CancellationToken();
+            await m_gatewayWebSocket.SendAsync(CreateSensorDataRequestMessage(sensorId), WebSocketMessageType.Text, true, ct);
+            
+            var responseBuffer = new ArraySegment<byte>();
+            await m_gatewayWebSocket.ReceiveAsync(responseBuffer, ct);
+
+            return ParseSensorData(responseBuffer);
+        }
+
+        private ISensorData ParseSensorData(ArraySegment<byte> socketData)
+        {
+            // STUB
+            return new SensorDataBase();
+        }
+
+        private ArraySegment<byte> CreateSensorDataRequestMessage(Guid sensorId)
+        {
+            // STUB
+            // Create JSON for this request
+            return new ArraySegment<byte>();
+        }
+    }
+}
